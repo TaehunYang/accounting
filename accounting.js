3dd46ce94e2636cc6aa698d46120b7f23741edf2
taehunyang <taehun21c@gmail.com>
2023-07-27 11:43:21 +0900

    create amortization table

--- accounting.js
+++ accounting.js
@@ -27,10 +27,69 @@ console.log("현할차", discount);
  * 711,780
  * 240,100
  * 2.401
- *              10%        12%     현할차       채권
- * 20230101                                 951,880 , 48,120
- * 20231231 100,000     114,226  14,226     966,106
- * 20241231 100,000     115,226  15,932     982,038
- * 20251231 100,000     117,844  17,962     1,000,000
+ *                          10%                eir 12%     현할차       채권
+ * startDay:20230101                                             presentValue: 951,880 , discount: 48,120
+ * middleDay:20231231   interest:100,000     114,226  14,226     966,106
+ * middleDay2:20241231  interest:100,000     115,226  15,932     982,038
+ * endDay:20251231      interest:100,000     117,844  17,962     securities: 1,000,000
  *
  * **/
+
+const amortization = [
+  {
+    day: startDay,
+    interest1: "",
+    interest2: "",
+    discount: "",
+    Value: presentValue,
+  },
+  {
+    day: middleDay,
+    interest1: interest,
+    interest2: Math.trunc(presentValue * eir),
+    discount: Math.trunc(presentValue * eir) - interest,
+    Value: presentValue + (Math.trunc(presentValue * eir) - interest),
+  },
+  {
+    day: middleDay2,
+    interest1: interest,
+    interest2: Math.trunc(
+      (presentValue + (Math.trunc(presentValue * eir) - interest)) * eir
+    ),
+    discount:
+      Math.trunc(
+        (presentValue + (Math.trunc(presentValue * eir) - interest)) * eir
+      ) - interest,
+    Value:
+      presentValue +
+      (Math.trunc(presentValue * eir) - interest) +
+      (Math.trunc(
+        (presentValue + (Math.trunc(presentValue * eir) - interest)) * eir
+      ) -
+        interest),
+  },
+  {
+    day: endDay,
+    interest1: interest,
+    interest2:
+      interest +
+      (securities -
+        (presentValue +
+          (Math.trunc(presentValue * eir) - interest) +
+          (Math.trunc(
+            (presentValue + (Math.trunc(presentValue * eir) - interest)) * eir
+          ) -
+            interest))),
+    discount:
+      securities -
+      (presentValue +
+        (Math.trunc(presentValue * eir) - interest) +
+        (Math.trunc(
+          (presentValue + (Math.trunc(presentValue * eir) - interest)) * eir
+        ) -
+          interest)),
+    Value: securities,
+  },
+];
+
+console.table(amortization);
